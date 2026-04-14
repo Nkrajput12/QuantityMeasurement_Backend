7843e9a9674c81b729915ea0ca14eadd9165e78e
Nkrajput12 <nand.kishor_cs22@gla.ac.in>
2026-04-14 13:46:25 +0530

    Update

---
+++ QuantityMeasurementRepoLayer/Migrations/20260414081615_FixDateTimeUtc.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace QuantityMeasurementRepoLayer.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixDateTimeUtc : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Timestamp",
+                table: "MeasurementHistory",
+                type: "timestamp without time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp with time zone");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<DateTime>(
+                name: "Timestamp",
+                table: "MeasurementHistory",
+                type: "timestamp with time zone",
+                nullable: false,
+                oldClrType: typeof(DateTime),
+                oldType: "timestamp without time zone");
+        }
+    }
+}
